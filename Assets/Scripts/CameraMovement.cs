25d90d231206cbea6f54c9cfd5b6e556527d8d29
Eric Ni <en1116@nyu.edu>
2021-04-03 18:39:54 -0400

    Altering movement and jumps
    Working on movement and jumps, dealing with bugs in the erraticness of movement. Also added car assets for the player to jump on

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;
+
+    public GameObject target;
+    private float targetDistance;
+
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 0.0f;
+    private float rotX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetDistance = Vector3.Distance(transform.position, target.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Taken from https://gamedevacademy.org/unity-3d-first-and-third-person-view-tutorial/#Section_2_Third_Person_Perspective
+        //This essentially moves the camera's rotation and position to remain around a good range for third person perspective. 
+        float y = Input.GetAxis("Mouse X") * turnSpeed;
+        rotX += Input.GetAxis("Mouse Y") * turnSpeed;
+
+
+        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+
+        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+
+        //Adjusts how close the camera is, by multiplying the distance by a constant. 
+        transform.position = target.transform.position - (transform.forward * targetDistance * 0.55f) + (transform.up * 2f);
+
+    }
+}
