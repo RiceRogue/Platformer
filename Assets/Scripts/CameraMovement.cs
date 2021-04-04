191acab241709844bfeabcd2947ef743465d655d
Eric Ni <en1116@nyu.edu>
2021-04-03 20:38:19 -0400

    BugFixes to Camera
    Chnages and fixed issues with camera rotation.  Currently, disabled the player movement because it has spottiness with its collisions, and also added the surrounding walls, floors, and a ship asset.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,15 +9,24 @@ public class CameraMovement : MonoBehaviour
     public GameObject target;
     private float targetDistance;
 
-    public float minTurnAngle = -90.0f;
-    public float maxTurnAngle = 0.0f;
+    public float minTurnAngleX;
+    public float maxTurnAngleX;
+    public float minTurnAngleY;
+    public float maxTurnAngleY;
+
     private float rotX;
+    private float rotY;
+
     // Start is called before the first frame update
     void Start()
     {
         targetDistance = Vector3.Distance(transform.position, target.transform.position);
+        minTurnAngleX = -50.0f;
+        maxTurnAngleX = 50.0f;
+        minTurnAngleY = -50.0f;
+        maxTurnAngleY = 50.0f;
 
-    }
+}
 
     // Update is called once per frame
     void Update()
@@ -27,13 +36,19 @@ public class CameraMovement : MonoBehaviour
         float y = Input.GetAxis("Mouse X") * turnSpeed;
         rotX += Input.GetAxis("Mouse Y") * turnSpeed;
 
+        //clamps the movement or angles of the camera
+        //Taken from https://gamedevacademy.org/unity-3d-first-and-third-person-view-tutorial/#Section_2_Third_Person_Perspective
+        //This essentially moves the camera's rotation and position to remain around a good range for third person perspective. 
 
-        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+        rotX = Mathf.Clamp(rotX, minTurnAngleX, maxTurnAngleX);
 
         transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
 
         //Adjusts how close the camera is, by multiplying the distance by a constant. 
-        transform.position = target.transform.position - (transform.forward * targetDistance * 0.55f) + (transform.up * 2f);
+        transform.position = target.transform.position - (transform.forward * targetDistance * 0.65f) + (transform.up * 2f);
+
 
     }
+
+    
 }
