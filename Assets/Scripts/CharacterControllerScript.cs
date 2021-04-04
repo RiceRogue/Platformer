014d26c19689593ca6ff06510b75d78cdb489374
Eric Ni <en1116@nyu.edu>
2021-04-04 14:24:41 -0400

    Astronaut + Cars
    Added a wall of cars and redesigned the ground floor of the level. Now, replaced the alien with an astronaut character, since the collisions and rigidbody had too many issues, and edited the code in the given astronaut movement code to fit the project and be written in my own style.

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    private CharacterController controller;
+
+    // Set in Editor
+    [SerializeField] float speed = 6.0f;
+    [SerializeField] float jumpSpeed = 8.0f;
+    [SerializeField] float gravity = 20.0f;
+
+
+    // Others
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Controls
+    private float xMove;
+    private float yMove;
+    private bool jump;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(xMove, 0.0f, yMove);
+            moveDirection *= speed;
+
+
+            // Face in dir of move
+            if (moveDirection.magnitude > float.Epsilon)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDirection * Time.fixedDeltaTime);
+            }
+
+
+            if (jump)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+
+
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+        jump = Input.GetButton("Jump");
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rgb;
+    public Camera cam;
+    
+    // Set in Editor
+    private float speed = 6.0f;
+    private float jumpSpeed = 0.5f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        bool jump = Input.GetKey(KeyCode.Space);
+
+
+
+        //Code from Nimso Ny's tutorial on moving the player relative to the camera. https://www.youtube.com/watch?v=ORD7gsuLivE 
+        Vector3 camForward = cam.transform.forward;
+        Vector3 camRight = cam.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        
+        camForward = camForward.normalized * z;
+        camRight = camRight.normalized * x;
+        //using transform position does not have as good of collisions, so I am using the movement from Rigidbody instead. 
+
+        rgb.AddForce((camForward + camRight) * speed);
+
+
+        //]
+
+        //transform.rotation = cam.transform.rotation;
+        
+
+
+        //jumping function, can only jump when near or touching a car. 
+
+        int platformLayer = LayerMask.GetMask("PlatformCar");
+
+        bool RaycastTouch = Physics.Raycast(transform.position, Vector3.down, jumpSpeed, platformLayer);
+        Debug.Log(RaycastTouch);
+        if (jump && RaycastTouch)
+        {
+            rgb.AddForce(cam.transform.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+
+
+    }
+   }
